f73e0486ff589d7456829b8b0789b568aa2aa8b1
Aditya Nagpal <adityanagpal10@gmail.com>
2025-04-28 03:26:37 +0530

    Aditya | progress

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,6 +3,6 @@
         <h1>Welcome to the Home Page</h1>
         <p>This is a simple home page.</p>
         <router-link to="/user/signup">Sign Up</router-link>
-        <router-link to="/user/signin">Sign In</router-link>
+        <router-link to="/user/login">Sign In</router-link>
     </div>
 </template>
